5500942eccf03f0a8db37e9dc5e4cece0a8a7c94
WhiskeyKiosk <fantomzed300@gmail.com>
2014-12-07 16:16:22 -0500

    git config --global user.email "you@example.com"   git config --global user.name "Your Name"

---
+++ Citadel/src/net/profkay/Window.java
@@ -0,0 +1,8 @@
+package net.profkay;
+
+public class Window 
+{
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
+
+}
---
+++ Citadel/src/net/states/States.java
@@ -0,0 +1,7 @@
+package net.states;
+
+public class States 
+{
+	public final static int GAME = 0;
+	public final static int MENU = 1;
+}
